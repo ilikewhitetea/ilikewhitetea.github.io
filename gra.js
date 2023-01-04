628e50e4e9a9f506943e3b6979e4b9dba124b8b2
ilikewhitetea <ilikewhitetea0@gmail.com>
2023-01-04 16:32:56 +0100

    test

--- gra.js
+++ gra.js
@@ -32,46 +32,5 @@ const balwan = add([
 ])
 
 function playSound(soundfile){
-    document.getElementById("sound").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\"/>";
+    document.getElementById("sound").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"true\"/>";
 }
-
-if(onclick(this(window))){// Your condition
-    playSound("muzyka.mp3"); // Location to your sound file
-}
-
-// const player = add([
-// 	sprite("balwan"),   
-// 	pos(120, 80),    
-
-// ])
-// const player = add([
-// 	sprite("3"),   
-// 	pos(120, 80),    
-// 	rotate(0),        
-// 	origin("center"), 
-
-
-// player.onUpdate(() => {
-
-// 	player.angle += 120 * dt()
-// })
-
-// for (let i = 0; i < 3; i++) {
-
-// 	const x = rand(0, width())
-// 	const y = rand(0, height())
-
-// 	add([
-// 		sprite("1"),
-// 		pos(x, y),
-// 	])
-//     add([
-// 		sprite("2"),
-// 		pos(x, y),
-// 	])
-//     add([
-// 		sprite("3"),
-// 		pos(x, y),
-// 	])
-
-// }
